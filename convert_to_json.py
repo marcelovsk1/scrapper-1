f1fa1a6c6ddd3e8c534da01abd3304fe8be5763f
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-27 10:45:35 -0300

    fixing bugs

--- convert_to_json.py
+++ convert_to_json.py
@@ -11,7 +11,7 @@ data = [
     {
       "title": "Birth of Planet Earth",
       "description": "How did Earth become a living planet as a result of the violent birth of our solar system?",
-      "date": "ANNUALLY IN DECEMBER",
+      "date": "Annually in December",
       "website": "https://espacepourlavie.ca/en/planetarium",
       "imageURL": "http://www.go-montreal.com/images/attractions/listings/festivals-events/birth-of-planet-earth.jpg"
     },
@@ -19,7 +19,7 @@ data = [
     {
       "title": "Montreal Bach Festival",
       "description": "Revel in the works of this musical giant in some of the swankiest spots in the city.",
-      "date": "ANNUALLY IN DECEMBER",
+      "date": "Annually in December",
       "website": "https://festivalbachmontreal.com/",
       "imageURL": "http://www.go-montreal.com/images/attractions/listings/festivals-events/montreal-bach-festival.jpg"
     },
