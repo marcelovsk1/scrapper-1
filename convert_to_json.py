b1e57724716f530d9c56d670b9c6869b143be937
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-27 09:59:00 -0300

    working on

--- convert_to_json.py
+++ convert_to_json.py
@@ -1,17 +1,28 @@
 import json
 
 data = [
-  {
-     "title": "Birth of Planet Earth",
-      "description": "How did Earth become a living planet as a result of the violent birth of our solar system?",
-      "website": "Website"
-  },
+    {
+      "Go Montreal Events": {
+      "webPage": "Montreal December Festival & Events",
+      "pageURL": "http://www.go-montreal.com/attraction_events_dec.htm"
+      }
+    },
 
-  {
-    "title": "Montreal Bach Festival",
-     "description": "Revel in the works of this musical giant in some of the swankiest spots in the city.",
-     "website": "Website"
-  }
+    {
+      "title": "Birth of Planet Earth",
+        "description": "How did Earth become a living planet as a result of the violent birth of our solar system?",
+        "date": "ANNUALLY IN DECEMBER",
+        "website": "https://espacepourlavie.ca/en/planetarium",
+        "imageURL": "http://www.go-montreal.com/images/attractions/listings/festivals-events/birth-of-planet-earth.jpg"
+    },
+
+    {
+      "title": "Montreal Bach Festival",
+      "description": "Revel in the works of this musical giant in some of the swankiest spots in the city.",
+      "date": "ANNUALLY IN DECEMBER",
+      "website": "https://festivalbachmontreal.com/",
+      "imageURL": "http://www.go-montreal.com/images/attractions/listings/festivals-events/montreal-bach-festival.jpg"
+    }
 ]
 
 json_data = json.dumps(data, indent=2)
