86378628f876b8e0e3f2ab7b67023a7e38f164b1
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-27 15:09:34 -0300

    working on

--- read.py
+++ read.py
@@ -2,13 +2,13 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = 'http://www.go-montreal.com/attraction_events_dec.htm'
+url = 'https://www.mtl.org/en/what-to-do'
 
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-div_read_post_list = soup.find('div', class_='read_post_list tabs_content')
+div_read_post_list = soup.find('div', class_='node-teaser__title')
 
 
 if div_read_post_list:
