5f91240bf53fa228015db55cacb56e6a7b9390b7
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-28 14:29:22 -0300

    working on

--- read.py
+++ read.py
@@ -8,7 +8,7 @@ r = requests.get(url)
 
 soup = BeautifulSoup(r.content, 'html.parser')
 
-div_read_post_list = soup.find('div', class_='node-teaser__title')
+div_read_post_list = soup.find('div', class_='read_post_list tabs_content')
 
 
 if div_read_post_list:
